8bc10ed995fa8eb705adada7873a66374c0c4780
Jason L Perry <jasper@ambethia.com>
2008-11-27 19:28:56 -0500

    Happy Thanksgiving

---
+++ APeonsBurden.lua
@@ -0,0 +1,24 @@
+local f, checkForUpdate = CreateFrame("Frame"), nil
+local WORK_COMPLETE = "Sound\\Creature\\Peon\\PeonBuildingComplete1.wav"
+
+f:RegisterEvent("QUEST_WATCH_UPDATE")
+f:RegisterEvent("QUEST_LOG_UPDATE")
+f:SetScript("OnEvent", function(self, event, ...)
+  if self[event] then return self[event](self, ...); end
+end)
+
+function f:QUEST_WATCH_UPDATE(questIndex)
+  checkForUpdate = 1
+end
+
+function f:QUEST_LOG_UPDATE(questIndex)
+  if checkForUpdate then
+    local allComplete = 1
+    for boardIndex = 1, GetNumQuestLeaderBoards(questIndex) do
+      local objComplete = select(3, GetQuestLogLeaderBoard(boardIndex, questIndex))
+      if not objComplete then allComplete = nil; end
+    end
+    if allComplete then PlaySoundFile(WORK_COMPLETE); end
+  end
+  checkForUpdate = nil
+end
